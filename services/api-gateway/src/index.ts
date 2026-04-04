380b3791e2103c72c23eb881a58ce7cea6d2af95
Al-Roman23 <alroman121523@gmail.com>
2026-04-05 00:56:34 +0600

    feat: initialize api-gateway service with Hono and account health check endpoint

--- services/api-gateway/src/index.ts
+++ services/api-gateway/src/index.ts
@@ -1,10 +1,16 @@
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
+import { getAccountHealth } from "./services/account.service";
 
 const app = new Hono();
 
 app.get("/", (c) => {
     return c.text("NovaPay API Gateway!");
+});
+
+app.get("/accounts/health", async (c) => {
+    const response = await getAccountHealth();
+    return c.json({ service: "account", response });
 });
 
 serve({

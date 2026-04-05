d732ee5d524191446cbc858853240f6fc84b3553
Al-Roman23 <alroman121523@gmail.com>
2026-04-05 16:27:25 +0600

    feat: initialize database package with Prisma and configure project structure

--- services/account-service/src/repositories/account.repository.ts
+++ services/account-service/src/repositories/account.repository.ts
@@ -1,4 +1,4 @@
-import { prisma } from "../prisma";
+import { prisma } from "@novapay/db";
 
 export const createAccount = async (userId: string, currency: string) => {
     return prisma.account.create({
